dcc61850b7600d68d9eb1d4115dbe255c8271a84
Tachibana446 <stinger33z@gmail.com>
2013-12-12 14:34:52 +0900

    ソースの編集
    黒のラインを検出できるようになった

--- src/Main.c
+++ src/Main.c
@@ -82,7 +82,11 @@ TASK(ActionTask){
 			caribration();
 			break;
 		case MODE_RUN:
-			
+			ecrobot_debug1(balancer.color_white, balancer.color_black , 0);
+			// 黒の値-50より大きければ音を鳴らす
+			if( ecrobot_get_light_sensor(NXT_PORT_S3) >= balancer.color_black-50 ){
+				ecrobot_sound_tone(720 , 200 , 10);
+			}
 			break;
 		default:
 			break;
