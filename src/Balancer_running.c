d676751647cf6e10550eff05dce9a77cb06debad
SuzukiTomoyuki <dxfsrs1004@gmail.com>
2014-01-09 18:03:16 +0900

    on/off

--- src/Balancer_running.c
+++ src/Balancer_running.c
@@ -21,6 +21,8 @@ void balance_running( Balancer *self ){
 				(F32)ecrobot_get_battery_voltage(),
 				&pwm_l,
 				&pwm_r);
+	nxt_motor_set_speed(NXT_PORT_C, pwm_l, 1);
+	nxt_motor_set_speed(NXT_PORT_B, pwm_r, 1);
 
 }
 
@@ -36,7 +38,7 @@ void set_color_black(Balancer *self ,int color_black){
 	self->color_black = color_black;
 }
 
-void calc_cmd_turn(Balancer* self){
+void calc_cmd_turn_on_off(Balancer* self){
 	int target_value = (self->color_white + self->color_black) / 2 ;	// 白と黒の中間値を出す
 	// 現在の輝度値が目標値より大きければ+50、そうでなければ-50の旋回値を与える
 	if(target_value > ecrobot_get_light_sensor(NXT_PORT_S3)){
@@ -46,4 +48,22 @@ void calc_cmd_turn(Balancer* self){
 		self->cmd_turn = -50;
 	}
 	
+}
+
+void calc_cmd_turn_pid(Balancer* self){
+	float deviation=0,i_deviation=0,d_deviation=0,bf_deviation=0;
+	float kp=2.0,ki=0.0,kd=1.0;
+	int target_value = (self->color_white + self->color_black) / 2 ;
+
+	deviation = target_value - ecrobot_get_light_sensor(NXT_PORT_S3);
+
+	i_deviation = i_deviation + (deviation * 0.004);
+	d_deviation = (bf_deviation - deviation)/0.004;
+	bf_deviation = deviation;
+
+	self->cmd_turn = kp*deviation + ki*i_deviation + kd*d_deviation;
+
+	if(self->cmd_turn>127)self->cmd_turn=127;
+	else if(self->cmd_turn<-127)self->cmd_turn=-127;
+
 }
--- src/Balancer_running.h
+++ src/Balancer_running.h
@@ -1,7 +1,6 @@
 #ifndef BALANCER
 #define BALANCER
 #include "Main.h"
-#include "balancer.h"
 
 typedef struct{
 	U32 gyro_offset;	
@@ -16,6 +15,7 @@ void balance_running(Balancer* self );
 void set_gyro_offset(Balancer* self ,U32 gyro_offset);
 void set_color_white(Balancer* self ,int color_white);
 void set_color_black(Balancer* self ,int color_black);
-void calc_cmd_turn(Balancer* self );
+void calc_cmd_turn_on_off(Balancer* self);
+void calc_cmd_turn_pid(Balancer* self);
 
 #endif
