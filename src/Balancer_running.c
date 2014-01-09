33c763564fb74ff0e5f2118e35f8cc06d5cc420f
Tachibana446 <stinger33z@gmail.com>
2014-01-09 14:44:08 +0900

    On/Off制御でライントレース

--- src/Balancer_running.c
+++ src/Balancer_running.c
@@ -4,6 +4,8 @@ void balancer_init( Balancer* self ){
 	self->gyro_offset = 0;
 	self->color_white = 0;
 	self->color_black = 0;
+	self->cmd_forward = 0;
+	self->cmd_turn    = 0;
 }
 
 void balance_running( Balancer *self ){
@@ -34,4 +36,16 @@ void set_color_white(Balancer *self , int color_white){
 
 void set_color_black(Balancer *self ,int color_black){
 	self->color_black = color_black;
+}
+
+void calc_cmd_turn(Balancer* self){
+	int target_value = (self->color_white + self->color_black) / 2 ;	// 白と黒の中間値を出す
+	// 現在の輝度値が目標値より大きければ+50、そうでなければ-50の旋回値を与える
+	if(target_value > ecrobot_get_light_sensor(NXT_PORT_S3)){
+		self->cmd_turn = 50;
+	}else
+	{
+		self->cmd_turn = -50;
+	}
+	
 }
--- src/Balancer_running.h
+++ src/Balancer_running.h
@@ -1,11 +1,13 @@
 #ifndef BALANCER
 #define BALANCER
-#include "main.h"
+#include "Main.h"
 
 typedef struct{
 	U32 gyro_offset;	
-	int color_white;
-	int color_black;
+	int color_white;	
+	int color_black;	
+	S8  cmd_forward;
+	S8  cmd_turn;
 }Balancer;
 
 void balancer_init(Balancer* self);
@@ -13,5 +15,6 @@ void balance_running(Balancer* self );
 void set_gyro_offset(Balancer* self ,U32 gyro_offset);
 void set_color_white(Balancer* self ,int color_white);
 void set_color_black(Balancer* self ,int color_black);
+void calc_cmd_turn(Balancer* self );
 
 #endif
